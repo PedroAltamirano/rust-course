b75ec44d12a073a10496701dcfc103b7e9b616f0
PedroAltamirano <pedroaal04@gmail.com>
2022-12-07 22:27:57 -0500

    modules

--- src/modules.rs
+++ src/modules.rs
@@ -1,38 +1,7 @@
-mod authentication {
-  use std::collections::hash_map::DefaultHasher;
-  use std::hash::{Hash, Hasher};
-
-  pub struct User {
-    username: String,
-    password_hash: u64,
-  }
-
-  fn hash_password<T: Hash>(psswd: &T) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    psswd.hash(&mut hasher);
-    hasher.finish()
-  }
-
-  impl User {
-    pub fn new(username: &str, password: &str) -> User {
-      User {
-        username: username.to_string(),
-        password_hash: hash_password(&password.to_owned()),
-      }
-    }
-
-    pub fn get_username(&self) -> &String {
-      &self.username
-    }
-
-    pub fn set_password(&mut self, new_password: &str) {
-      self.password_hash = hash_password(&new_password.to_owned())
-    }
-  }
-}
+mod mod_auth;
 
 fn main() {
-  let user = authentication::User::new("jeremy", "super-secret");
+  let mut user = mod_auth::User::new("jeremy", "super-secret");
 
   println!("The username is: {}", user.get_username());
   user.set_password("even-more-secret");
